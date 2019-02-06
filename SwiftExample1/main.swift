686164c123c95b3ae944be323fa373e1107f84ee
Eric <emitchell008@gmail.com>
2019-02-06 12:12:29 -0600

    First commit

---
+++ SwiftExample1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExample1
+//
+//  Created by Eric Mitchell on 2/5/19.
+//  Copyright © 2019 Eric Mitchell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
